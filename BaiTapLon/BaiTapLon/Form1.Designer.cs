b07f9f739bfbe52879b430469bde29562a27906a
LeTheAnhNhan <36005853+LeTheAnhNhan@users.noreply.github.com>
2018-04-26 20:56:07 +0700

    sdadsad

---
+++ BaiTapLon/BaiTapLon/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace BaiTapLon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(8, 41);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(445, 263);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(437, 237);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "System Info";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(4, 8);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(432, 225);
+            this.listBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(437, 237);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Process Info";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(2, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(434, 234);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Sesstion ID";
+            this.columnHeader1.Width = 73;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Process ID";
+            this.columnHeader2.Width = 68;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Process Name";
+            this.columnHeader3.Width = 83;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ghi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 350);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ BaiTapLon/BaiTapLon/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+[StructLayout(LayoutKind.Sequential)]
+//Struct nhận thông tin hệ thống
+public struct SYSTEM_INFO
+
+{
+
+    public uint dwOemId;
+    public uint dwPageSize;
+    public uint lpMinimumApplicationAddress;
+    public uint lpMaximumApplicationAddress;
+    public uint dwActiveProcessorMask;
+    public uint dwNumberOfProcessors;
+    public uint dwProcessorType;
+    public uint dwAllocationGranularity;
+    public uint dwProcessorLevel;
+    public uint dwProcessorRevision;
+}
+
+//struct nhận trạng thái bộ nhớ
+[StructLayout(LayoutKind.Sequential)]
+public struct MEMORYSTATUS
+{
+    public uint dwLength;
+    public uint dwMemoryLoad;
+    public uint dwTotalPhys;
+    public uint dwAvailPhys;
+    public uint dwTotalPageFile;
+    public uint dwAvailPageFile;
+    public uint dwTotalVirtual;
+    public uint dwAvailVirtual;
+}
+
+namespace BaiTapLon
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            #region hàm
+            try
+            {
+                SYSTEM_INFO pSI = new SYSTEM_INFO();
+                GetSystemInfo(ref pSI);
+                string CPUType = "unknown";
+                switch (pSI.dwProcessorType)
+                {
+                    
+                    case PROCESSOR_INTEL_386:
+                        {
+                            CPUType = "Intel 386";
+                            break;
+                        }
+
+                    case PROCESSOR_INTEL_486:
+                        {
+                            CPUType = "Intel 486";
+                            break;
+                        }
+
+                    case PROCESSOR_INTEL_PENTIUM:
+                        {
+                            CPUType = "Intel Pentium";
+                            break;
+                        }
+
+                    case PROCESSOR_MIPS_R4000:
+                        {
+                            CPUType = "MIPS R4000";
+                            break;
+                        }
+
+                    case PROCESSOR_ALPHA_21064:
+                        {
+                            CPUType = "DEC Alpha 21064";
+                            break;
+                        }                       
+                }
+
+                //hiển thị thông tin
+                listBox1.Items.Insert(0, "Active Processor Mask :		" + pSI.dwActiveProcessorMask.ToString());
+                listBox1.Items.Insert(1, "Allocation Granularity :		" + pSI.dwAllocationGranularity.ToString());
+                listBox1.Items.Insert(2, "Number Of Processors :		" + pSI.dwNumberOfProcessors.ToString());
+                listBox1.Items.Insert(3, "OEM ID :				" + pSI.dwOemId.ToString());
+                listBox1.Items.Insert(4, "Page Size :			" + pSI.dwPageSize.ToString());
+                // Processor Level (Req filtering to get level)
+                listBox1.Items.Insert(5, "Processor Level Value :		" + pSI.dwProcessorLevel.ToString());
+                listBox1.Items.Insert(6, "Processor Revision :		" + pSI.dwProcessorRevision.ToString());
+                listBox1.Items.Insert(7, "CPU type :			" + CPUType);
+                listBox1.Items.Insert(8, "Maximum Application Address :	" + pSI.lpMaximumApplicationAddress.ToString());
+                listBox1.Items.Insert(9, "Minimum Application Address :	" + pSI.lpMinimumApplicationAddress.ToString());
+
+                /**************	lấy thông tin từ trạng thái bộ nhớ ****************/
+
+                MEMORYSTATUS memSt = new MEMORYSTATUS();
+                GlobalMemoryStatus(ref memSt);
+
+                listBox1.Items.Insert(10, "Available Page File :		" + (memSt.dwAvailPageFile / 1024).ToString());
+                listBox1.Items.Insert(11, "Available Physical Memory :		" + (memSt.dwAvailPhys / 1024).ToString());
+                listBox1.Items.Insert(12, "Available Virtual Memory :		" + (memSt.dwAvailVirtual / 1024).ToString());
+                listBox1.Items.Insert(13, "Size of structur :			" + memSt.dwLength.ToString());
+                listBox1.Items.Insert(14, "Memory In Use :			" + memSt.dwMemoryLoad.ToString());
+                listBox1.Items.Insert(15, "Total Page Size :			" + (memSt.dwTotalPageFile / 1024).ToString());
+                listBox1.Items.Insert(16, "Total Physical Memory :		" + (memSt.dwTotalPhys / 1024).ToString());
+                listBox1.Items.Insert(17, "Total Virtual Memory :		" + (memSt.dwTotalVirtual / 1024).ToString());
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process theprocess in processlist)
+            {
+                ListViewItem SessionID = new ListViewItem(theprocess.SessionId.ToString());
+                ListViewItem.ListViewSubItem ProcessID = new ListViewItem.ListViewSubItem(SessionID, theprocess.Id.ToString());
+                ListViewItem.ListViewSubItem ProcessName = new ListViewItem.ListViewSubItem(SessionID, theprocess.ProcessName.ToString());
+                SessionID.SubItems.Add(ProcessID);
+                SessionID.SubItems.Add(ProcessName);
+                listView1.Items.Add(SessionID);
+            }
+            #endregion
+        }
+        //hằng số cho cho các bộ sử lý
+        public const int PROCESSOR_INTEL_386 = 386;
+        public const int PROCESSOR_INTEL_486 = 486;
+        public const int PROCESSOR_INTEL_PENTIUM = 586;
+        public const int PROCESSOR_MIPS_R4000 = 4000;
+        public const int PROCESSOR_ALPHA_21064 = 21064;
+
+        //lấy thông tin hệ thống
+        [DllImport("kernel32")]
+        static extern void GetSystemInfo(ref SYSTEM_INFO pSI);
+
+        //lấy trạng thái bộ nhớ
+        [DllImport("kernel32")]        
+        static extern void GlobalMemoryStatus(ref MEMORYSTATUS buf);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(File.Exists("./Thông tin.txt"))
+            {
+                File.Delete("./Thông tin.txt");
+            }
+            //tại file txt
+            using (StreamWriter ghi = File.CreateText("Thông Tin.txt"))
+            {
+                foreach(string i in listBox1.Items)
+                {
+                    ghi.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
